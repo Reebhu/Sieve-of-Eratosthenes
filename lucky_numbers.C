ba53f6ad8913956b8049ee4862367cb98f83e3fa
Reebhu <40407843+Reebhu@users.noreply.github.com>
2019-11-03 02:58:37 +0530

    Create lucky_numbers.C

---
+++ lucky_numbers.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,j,n;
+    scanf("%d",&n);
+    int a[100],b[100];
+    for(i=0;i<n;i++)
+    {
+        a[i]=i+1;
+        b[i]=1;    
+    }
+    
+    for(i=1;i<=n;i++)
+    {
+        if(b[i]==1)
+        {
+        int k =a[i];
+        int count =0;
+        for(j=0;j<n;j++)
+        {
+            if(b[j]==1)
+            {
+                count++;
+                if(count==k)
+                {
+                    b[j]=0;
+                    count=0;
+                }
+            }
+        }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(b[i]==1)
+            printf("%d ",a[i]);
+    }
+   
+	return 0;
+} 
